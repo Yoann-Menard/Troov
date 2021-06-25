4218b5af29f1b176c08053ee4228461f2e3fcfce
Yoann <yoann.menard@epitech.eu>
2021-06-25 05:48:58 +0200

    Validation du Schema User avec Joi

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,25 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
-
-const Joi = require('joi');
-
-const schema = Joi.object({
-  username: Joi.string().alphanum().min(6).max(255).required(),
-  email: Joi.string()
-    .min(6)
-    .max(255)
-    .required()
-    .email({
-      minDomainSegments: 2,
-      tlds: { allow: ['com', 'net'] },
-    }),
-  password: Joi.string()
-    .min(6)
-    .max(1024)
-    .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
-});
-
+const { registerValidation, loginValidation } = require('../validation');
 router.get('/', async (req, res) => {
   try {
     const users = await User.find().limit(15);
@@ -39,26 +21,21 @@ router.get('/:userId', async (req, res) => {
 });
 
 router.post('/register', async (req, res) => {
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const user = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  });
   try {
-    const value = await schema.validateAsync(req.body);
-    res.send(value);
+    const savedUser = await user.save();
+    res.json(savedUser);
   } catch (err) {
     res.json({ message: err });
   }
 });
-
-//   const user = new User({
-//     username: req.body.username,
-//     email: req.body.email,
-//     password: req.body.password,
-//   });
-//   try {
-//     const savedUser = await user.save();
-//     res.json(savedUser);
-//   } catch (err) {
-//     res.json({ message: err });
-//   }
-// });
 
 // router.post('/login', async (req, res) => {
 //     const user = new User({
