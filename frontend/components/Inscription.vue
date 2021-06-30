ae63dcf93696d8b50570eadebd789838dc31481b
Yoann <yoann.menard@epitech.eu>
2021-06-30 22:15:17 +0200

    [Test] Utilisation du fetch hook affichage des users provenant du UserSchema(backend)

--- frontend/components/Inscription.vue
+++ frontend/components/Inscription.vue
@@ -1,9 +1,26 @@
 <template>
-  <div></div>
+  <p v-if="$fetchState.pending">Fetching users...</p>
+  <p v-else-if="$fetchState.error">An error occurred :(</p>
+  <div v-else>
+    <h1>Nuxt Users</h1>
+    <ul>
+      <li v-for="user of users" :key="user.id">{{ user.username }}</li>
+    </ul>
+    <button @click="$fetch">Refresh</button>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data () {
+    return {
+      users: []
+    }
+  },
+  async fetch () {
+    this.users = await fetch(
+      'http://localhost:6300/users'
+    ).then(res => res.json())
+  }
+}
 </script>
-
-<style lang="scss" scoped></style>
